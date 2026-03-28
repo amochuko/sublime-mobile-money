e95ff7c97d021fbd2ebf5e0ef425bcb522a1b2f7
amochuko <7204868+amochuko@users.noreply.github.com>
2026-03-28 17:34:14 +0100

    add: impl tokenController.delete

--- src/controllers/tokenController.ts
+++ src/controllers/tokenController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
 import { pool } from "../config/database";
+import { redisClient } from "../config/redis";
 
 export const tokenController = {
-    // List all active refresh tokens for current user
+  // List all active refresh tokens for current user
   findAll: async (req: Request, res: Response) => {
     const userId = (req as any).user.id || (req as any).user_id;
 
@@ -19,5 +20,55 @@ export const tokenController = {
 
     return result.rows;
   },
+  // List token by its Id
+  delete: async (req: Request, res: Response) => {
+    const tokenId = req.params.tokenId;
+    const userId = (req as any).user.id || (req as any).user_id;
 
+    const client = await pool.connect();
+
+    try {
+      const result = await client.query(
+        `SELECT token_jti FROM refresh_tokens
+            WHERE id = $1 AND user_id = $2`,
+        [tokenId, userId],
+      );
+
+      if (result.rows.length === 0) {
+        return res.status(400).json({
+          success: false,
+          error: "Token not found",
+        });
+      }
+
+      const { token_jti } = result.rows[0];
+
+      await client.query("BEGIN");
+
+      // Update DB
+      await pool.query(
+        `UPDATE refresh_tokens 
+        SET revoked_at = NOW(), is_active = FALSE 
+        WHERE id = $1`,
+        [tokenId],
+      );
+
+      // Clear from Redis
+      await redisClient.del(`refresh_token:${token_jti}`);
+
+      await pool.query("COMMIT");
+
+      res.json({
+        success: true,
+        message: "Token revoked successfully",
+      });
+    } catch (err: any) {
+      await client.query("ROLLBACK");
+      console.error(err);
+
+      res.status(500).json({ success: false, error: err.message });
+    } finally {
+      client.release();
+    }
+  },
 };
